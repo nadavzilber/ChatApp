9629b1205e8247635a57fbd4f86b23a3f727aea3
nadavzilber <nadavzilber@gmail.com>
2020-11-17 22:28:26 +0200

    added sender and clients

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+let state = { clients: {} };
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  io.emit('chat message', 'user connected');
+    console.log('a user connected');
+    const { id } = socket.client;
+    state.clients[id] = { name: id };
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-    io.emit('chat message', 'user disconnected.');
-  });
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-  });
+    io.emit('chat message', {
+        sender: 'System',
+        text: 'user connected: ' + id
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('chat message', {
+            sender: 'System',
+            text: 'user disconnected.'
+        });
+    });
+
+    socket.on('chat message', (msg) => {
+        const { id } = socket.client;
+        const clientName = state.clients[id].name;
+        io.emit('chat message', {
+            sender: clientName,
+            text: msg
+        });
+    });
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
